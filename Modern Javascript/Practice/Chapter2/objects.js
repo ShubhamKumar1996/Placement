ebd392ef78c6e9238a6f12031cdba95d9ddef0e8
shubham <nimcetshubhamkumar@gmail.com>
2020-05-06 22:41:19 +0530

    checking

---
+++ Modern Javascript/Practice/Chapter2/objects.js
@@ -0,0 +1,9 @@
+user = {
+    name: "shubham",
+    age: 24,
+    email: "badkadad@gmail.com",
+    username: "1996Shaitaan",
+    blogs: ["This is Working", "That is not working"]
+};
+
+console.log(user);
